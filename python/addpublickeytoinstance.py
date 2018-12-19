dfd7707c71d7b423f94f3f180c8e083847148aa5
Rodriguez, Lewis <lewis.rodriguez@one.verizon.com>
2018-12-19 18:00:35 -0400

    First script push.

--- python/addpublickeytoinstance.py
+++ python/addpublickeytoinstance.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 #############################################
 # *Developed with python 2.7*               #
--- python/checkdiskspace.py
+++ python/checkdiskspace.py
@@ -1,20 +1,28 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import subprocess
 
-#Variables
+
+
+def mail_sender():
+    print "test"
+
+
+#Starting point 
 output = subprocess.Popen(['df','-h'], stdout = subprocess.PIPE)
-list = output.communicate()[0].split("\n")
-list_lenght = len(list)
-#print list
+output_list = output.communicate()[0].split("\n")
+list_lenght = len(output_list)
 exceeded = {}
 
 counter = 0
-for i in (list[1:]):
+for i in (output_list[1:]):
     if i:
         partition =  i.split()[0]
         percentage = int(i.split()[4].replace("%", ""))
         if percentage > 80:
-            exceeded[counter] = {partition:percentage}
+            exceeded[counter] = {
+                                partition: percentage
+                                }
 #            print counter
             counter += 1
+print exceeded[0]
