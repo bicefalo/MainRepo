5582a208433bfb958c668da3a5d48dda640709df
Rodriguez, Lewis <lewis.rodriguez@one.verizon.com>
2018-12-19 17:05:22 -0400

    First script push.

--- python/checkdiskspace.py
+++ python/checkdiskspace.py
@@ -2,6 +2,18 @@
 
 import subprocess
 
+#Variables
 output = subprocess.Popen(['df','-h'], stdout = subprocess.PIPE)
-print output.communicate()
+list = output.communicate()[0].split("\n")
+list_lenght = len(list)
+#print list
+exceeded = {}
 
+for i in (list[1:]):
+    if i:
+        partition =  i.split()[0]
+        percentage = int(i.split()[4].replace("%", ""))
+        if percentage > 80:
+            print partition, percentage
+
+print exceeded
