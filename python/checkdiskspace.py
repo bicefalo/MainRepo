46bc2380c405c22c8095e4292c6c3fa5c744cfba
Rodriguez, Lewis <lewis.rodriguez@one.verizon.com>
2018-12-19 19:00:36 -0400

    Adding email notification

--- python/checkdiskspace.py
+++ python/checkdiskspace.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 
 import subprocess
+from socket import gethostname
 
 
-
-def mail_sender():
-    print "test"
-
+def mail_sender(exceeded):
+    print exceeded
 
 #Starting point 
 output = subprocess.Popen(['df','-h'], stdout = subprocess.PIPE)
 output_list = output.communicate()[0].split("\n")
 list_lenght = len(output_list)
 exceeded = {}
+host_name = gethostname()
 
 counter = 0
 for i in (output_list[1:]):
@@ -21,9 +21,13 @@ for i in (output_list[1:]):
         percentage = int(i.split()[4].replace("%", ""))
         if percentage > 80:
             exceeded[counter] = {
-                                partition: percentage
+                                    partition: percentage
                                 }
 #            print counter
             counter += 1
-print exceeded[0]
+
+if  exceeded:
+    mail_sender(exceeded)
+else:
+    print "No file systems over 80% of capacity."
 
