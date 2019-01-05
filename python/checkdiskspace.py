f1a81ab564dcb80217fa21b35b4c50cb53316dad
Lewis Rodriguez <l.rodriguez.contrera@gmail.com>
2019-01-05 01:11:22 -0400

    Adding email support.

--- python/checkdiskspace.py
+++ python/checkdiskspace.py
@@ -7,82 +7,66 @@ from email.mime.multipart import MIMEMultipart
 
 
 def mail_sender(exceeded, host_name):
-    sender = "l.rodriguez.contrera@gmail.com"
-    receiver = ""
-    with open("/tmp/report", 'w+') as report:
-        head_html = """
-        <html>
-	<head>
-	<style>
+    try:
+        email_server = smtplib.SMTP('smtp.gmail.com', 587)
+        email_server.ehlo()
+        sender = "l.rodriguez.contrera@gmail.com"
+        receiver = "l.rodriguez.contrera@gmail.com"
+        with open("/tmp/report", 'w') as report:
+            html_header = '''<!DOCTYPE html>
+            <html>
+            <head>
+            <style>
+            table {
+              border-collapse: collapse;
+            }
+            
+            th, td {
+              text-align: center;
+              padding: 8px;
+            }
+            
+            tr:nth-child(even){background-color: #f2f2f2}
+            
+            th {
+              background-color: #4CAF50;
+              color: white;
+            }
+            </style>
+            </head>
+            <body>
+           '''
 
-	table {
-	    border-collapse: collapse;
- 	    width: 100%;
-		}
+            html_body = '''
+            <tr>
+                <th>Partition</th>
+                <th>Percentage</th>
+              </tr>'''
 
-	.child_style {
-	border: 0px;
-		}
+            html_botton = '''
+            </table>
+            </body>
+            </html>
+           '''
+            report.write(html_header)
+            report.write("<h2>" + host_name + " </h2>")
+            report.write("<table>")
+            report.write(html_body)
+            for key, value in exceeded.items():
+                print key, value
+                report.write("<tr>")
+                report.write("<td>" + key + "</td>")
+                report.write("<td>" + str(value) + "%</td>")
+                report.write("</tr>") 
+            report.write(html_botton)
+    except Exception as  e:
+        print "There was an error trying to send the report email, please check."
+        print e
 
-	th, td {
-    	text-align: left;
-    	border: 1px solid #999;
-    	padding: 0px;
-	}
 
-	th {
-    	background-color: #abd28e;
-    	color: black;
-    	border: none;
-    	font-weight:normal;
-	}
 
-	tr:hover {
-        	  background-color: #ffff99;
-        	}
-
-	td {
-    	border-collapse: collapse;
-    	border-color: #9dcc7a;
-	}
-
-	</style>
-	</head>
-	<body>
-	<table style="width:100%">
-        """
-
-        botton_html = """
-        </table>
-        </td>
-        </table>
-        </body>
-        </html>
-        """  
-        
-        report.write(head_html) 
-        
-        <tr><td>kevin.m.bonanno</td>
-        <td>
-        <table class="child_style" style="width:100%">
-        <tr>
-        <th width="90%">Comment: "updates",   Jira Ticket: <a href="http://vectrack.verizon.com/browse/PQ-23">PQ-23</a> </th>
-        <th width="20%">Issue #: 31861</th>
-        </tr>
-        <tr>
-        <td colspan="2">vecbusservices/ici/src/main/java/com/vz/ici/service/batch/IciInboundEmailService.java</td>
-        </tr>
-        <tr>
-        <td colspan="2">vecbusservices/ici/src/main/java/com/vz/ici/util/IciUtil.java</td>
-        </tr>
-
-    #msg = MIMEText(report.read())
-    #msg['Subject'] = "[WARNING] Diskspace report for server " + host_name
-    #msg['From'] = "l.rodriguez.contrera@gmail.com"
-    #msg['To'] = "erick.casado@verizon.com"
-    #message = smtplib.SMTP('localhost')
-    #message.sendmail("l.rodriguez.contrera@gmail.com", "erick.casado@verizon.com", msg.as_string())
-    #message.quit()
+            
+            
 
 #Starting point 
 output = subprocess.Popen(['df','-h'], stdout = subprocess.PIPE)
@@ -91,17 +75,12 @@ list_lenght = len(output_list)
 exceeded = {}
 host_name = gethostname()
 
-counter = 0
 for i in (output_list[1:]):
     if i:
         partition =  i.split()[0]
         percentage = int(i.split()[4].replace("%", ""))
         if percentage > 80:
-            exceeded[counter] = {
-                                    partition: percentage
-                                }
-#            print counter
-            counter += 1
+            exceeded.update({partition: percentage})
 
 if  exceeded:
     mail_sender(exceeded, host_name)
